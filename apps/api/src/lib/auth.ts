c1412bde296b9e72c41ced7856610e453ff7a471
shubham-021 <shubham.arka@gmail.com>
2025-12-29 23:52:13 +0530

    added oauth google/github

--- apps/api/src/lib/auth.ts
+++ apps/api/src/lib/auth.ts
@@ -1,6 +1,9 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@once/database";
+import { config } from "dotenv";
+
+config({ path: "../../../../.env" });
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
@@ -8,5 +11,15 @@ export const auth = betterAuth({
     }),
     emailAndPassword: {
         enabled: true
-    }
+    },
+    // socialProviders: {
+    //     google: {
+    //         clientId: process.env.GOOGLE_CLIENT_ID!,
+    //         clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    //     },
+    //     github: {
+    //         clientId: process.env.GITHUB_CLIENT_ID!,
+    //         clientSecret: process.env.GITHUB_CLIENT_SECRET!
+    //     }
+    // }
 })
